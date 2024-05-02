cab168af58cbba2fc3e5f792f4bfbbd647035bf2
appbsm <168729489+appbsm@users.noreply.github.com>
2024-05-02 18:12:08 +0700

    Add files via upload

---
+++ batch_files/due_policy.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files (x86)\PHP\v5.3\php.exe" "C:\inetpub\wwwroot\broker\includes\fx_auto_run_date.php"
+
---
+++ batch_files/due_policy_mychelle.bat
@@ -0,0 +1,2 @@
+cd "C:\Program Files\Curl\bin"
+curl --insecure -v http://brokerapp.installdirect.asia:8742/includes/fx_auto_run_date.php
